8ff6cf303bc4e13d487f67d4a5360b77dc2bd735
Aakash <143587104+akhockey59@users.noreply.github.com>
2024-10-05 00:45:52 +0530

    Update services.js

--- frontend/src/utils/services.js
+++ frontend/src/utils/services.js
@@ -1,4 +1,4 @@
-export const  baseUrl = "http://localhost:3000/api"
+export const  baseUrl = "https://video-calling-rf98.onrender.com"
 
 export const postRequest = async(url, body) =>{
    const response =  await fetch(url, {
